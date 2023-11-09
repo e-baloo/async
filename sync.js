f70cabf523df0a2a2f98f6acb4d037c285329b6d
Marc DONVAL <marc.donval@e-baloo.com>
2023-11-09 09:11:12 +0100

    Update sync.js

--- sync.js
+++ sync.js
@@ -1,9 +1,9 @@
 const fs = require("node:fs");
 
  try {
-    const files = await fs.readdirSync(".");
+    const files = fs.readdirSync(".");
     console.log("files: ", files);
-    const content = await fs.readFileSync(files[0], { encoding: "utf-8" });
+    const content = fs.readFileSync(files[0], { encoding: "utf-8" });
     console.log("content: ", content);
   } catch (err) {
     console.log("err: ", err);
